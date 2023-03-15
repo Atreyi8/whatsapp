d6cf49bedd690b6d0fad43b99635da7b255b3f35
Atreyi8 <79186980+Atreyi8@users.noreply.github.com>
2023-03-15 19:38:35 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,541 @@
+let data = [
+  {
+    id: 1,
+    title: "Diya",
+    lastRead: "10:59",
+    imageURL: "../p1.jpg",
+    orderId: "OD123",
+    messageList: [
+      {
+        messageId: "msg1",
+        message: "Hi",
+        messageType: "text",
+        messageReceived: "false",
+        time: "2:59"
+      },
+      {
+        messageId: "msg2",
+        message: "need assistance",
+        messageType: "text",
+        messageReceived: "true",
+        time: "3:59"
+      }
+    ]
+  },
+  {
+    id: 2,
+    title: "Riya",
+    imageURL: "../p2.jpg",
+    orderId: "OD1234",
+    lastRead: "8:34",
+    messageList: [
+      {
+        messageId: "msg1",
+        message:
+          "text h toh h bhai text h toh h bhai text h toh h bhai text h toh h bhai text h toh h bhai",
+        messageType: "text",
+        messageReceived: "tue",
+        time: "00:59"
+      },
+      {
+        messageId: "msg2",
+        message: "need assistance",
+        messageType: "text nhi h",
+        messageReceived: "false",
+        time: "2:09"
+      }
+    ]
+  },
+  {
+    id: 3,
+    title: "Siya",
+    imageURL: "../p3.jpg",
+    orderId: "OD1234",
+    lastRead: "8:34",
+    messageList: [
+      {
+        messageId: "msg1",
+        message:
+          "text h toh h bhai text h toh h bhai text h toh h bhai text h toh h bhai text h toh h bhai",
+        messageType: "text",
+        messageReceived: "false",
+        time: "1:59"
+      },
+      {
+        messageId: "msg2",
+        message: "need assistance",
+        messageType: "text nhi h",
+        messageReceived: "true",
+        time: "2:59"
+      }
+    ]
+  },
+  {
+    id: 4,
+    title: "Harsh",
+    imageURL: "../p4.jpg",
+    orderId: "OD1234",
+    lastRead: "8:34",
+    messageList: [
+      {
+        messageId: "msg1",
+        message:
+          "text h toh h bhai text h toh h bhai text h toh h bhai text h toh h bhai text h toh h bhai",
+        messageType: "text",
+        messageReceived: "false",
+        time: "1:00"
+      },
+      {
+        messageId: "msg2",
+        message: "need assistance",
+        messageType: "text nhi h",
+        messageReceived: "false",
+        time: "14:59"
+      }
+    ]
+  },
+  {
+    id: 5,
+    title: "Atreyi",
+    imageURL: "../p5.jpg",
+    orderId: "OD1234",
+    lastRead: "8:34",
+    messageList: [
+      {
+        messageId: "msg1",
+        message:
+          "text h toh h bhai text h toh h bhai text h toh h bhai text h toh h bhai text h toh h bhai",
+        messageType: "text",
+        messageReceived: "true",
+        time: "2:59"
+      },
+      {
+        messageId: "msg2",
+        message: "need assistance",
+        messageType: "text nhi h",
+        messageReceived: "true",
+        time: "2:59"
+      }
+    ]
+  },
+  {
+    id: 6,
+    title: "ira",
+    imageURL: "../p1.jpg",
+    orderId: "OD1234",
+    lastRead: "8:34",
+    messageList: [
+      {
+        messageId: "msg1",
+        message:
+          "text h toh h bhai text h toh h bhai text h toh h bhai text h toh h bhai text h toh h bhai",
+        messageType: "text",
+        messageReceived: "false",
+        time: "6:59"
+      },
+      {
+        messageId: "msg2",
+        message: "text nhi h",
+        messageType: "text nhi h",
+        messageReceived: "true",
+        time: "20:59"
+      },
+      {
+        messageId: "msg3",
+        message: "thwbdkwj assistance",
+        messageType: "thwbdkwj",
+        messageReceived: "false",
+        time: "20:59"
+      },
+      {
+        messageId: "msg3",
+        message: "need dvcgxhbjsdknlm",
+        messageType: "dvcgxhbjsdknlm,x",
+        messageReceived: "true",
+        time: "20:59"
+      }
+    ]
+  },
+  {
+    id: 7,
+    title: "Sella",
+    imageURL: "../p2.jpg",
+    orderId: "OD1234",
+    lastRead: "12:34",
+    messageList: [
+      {
+        messageId: "msg1",
+        message:
+          "text h toh h bhai text h toh h bhai text h toh h bhai text h toh h bhai text h toh h bhai",
+        messageType: "text",
+        messageReceived: "false",
+        time: "2:59"
+      },
+      {
+        messageId: "msg2",
+        message: "need assistance",
+        messageType: "text nhi h",
+        messageReceived: "true",
+        time: "8:59"
+      }
+    ]
+  },
+  {
+    id: 8,
+    title: "Harsh",
+    imageURL: "../p4.jpg",
+    orderId: "OD1234",
+    lastRead: "8:34",
+    messageList: [
+      {
+        messageId: "msg1",
+        message:
+          "text h toh h bhai text h toh h bhai text h toh h bhai text h toh h bhai text h toh h bhai",
+        messageType: "text",
+        messageReceived: "false",
+        time: "1:00"
+      },
+      {
+        messageId: "msg2",
+        message: "need assistance",
+        messageType: "text nhi h",
+        messageReceived: "false",
+        time: "14:59"
+      }
+    ]
+  },
+  {
+    id: 9,
+    title: "Atreyi",
+    imageURL: "../p5.jpg",
+    orderId: "OD1234",
+    lastRead: "8:34",
+    messageList: [
+      {
+        messageId: "msg1",
+        message:
+          "text h toh h bhai text h toh h bhai text h toh h bhai text h toh h bhai text h toh h bhai",
+        messageType: "text",
+        messageReceived: "true",
+        time: "2:59"
+      },
+      {
+        messageId: "msg2",
+        message: "need assistance",
+        messageType: "text nhi h",
+        messageReceived: "true",
+        time: "2:59"
+      }
+    ]
+  },
+  {
+    id: 10,
+    title: "Netra",
+    imageURL: "../p1.jpg",
+    orderId: "OD1234",
+    lastRead: "8:34",
+    messageList: [
+      {
+        messageId: "msg1",
+        message:
+          "text h toh h bhai text h toh h bhai text h toh h bhai text h toh h bhai text h toh h bhai",
+        messageType: "text",
+        messageReceived: "false",
+        time: "6:59"
+      },
+      {
+        messageId: "msg2",
+        message: "need assistance",
+        messageType: "text nhi h",
+        messageReceived: "false",
+        time: "20:59"
+      }
+    ]
+  },
+  {
+    id: 11,
+    title: "Sella",
+    imageURL: "../p2.jpg",
+    orderId: "OD1234",
+    lastRead: "8:34",
+    messageList: [
+      {
+        messageId: "msg1",
+        message:
+          "text h toh h bhai text h toh h bhai text h toh h bhai text h toh h bhai text h toh h bhai",
+        messageType: "text",
+        messageReceived: "false",
+        time: "2:59"
+      },
+      {
+        messageId: "msg2",
+        message: "need assistance",
+        messageType: "text nhi h",
+        messageReceived: "true",
+        time: "8:59"
+      }
+    ]
+  },
+  {
+    id: 12,
+    title: "aarna",
+    imageURL: "../p1.jpg",
+    orderId: "OD1234",
+    lastRead: "8:34",
+    messageList: [
+      {
+        messageId: "msg1",
+        message:
+          "text h toh h bhai text h toh h bhai text h toh h bhai text h toh h bhai text h toh h bhai",
+        messageType: "text",
+        messageReceived: "false",
+        time: "6:59"
+      },
+      {
+        messageId: "msg2",
+        message: "need assistance",
+        messageType: "text nhi h",
+        messageReceived: "false",
+        time: "20:59"
+      }
+    ]
+  },
+  {
+    id: 13,
+    title: "Sella",
+    imageURL: "../p2.jpg",
+    orderId: "OD1234",
+    lastRead: "8:34",
+    messageList: [
+      {
+        messageId: "msg1",
+        message:
+          "text h toh h bhai text h toh h bhai text h toh h bhai text h toh h bhai text h toh h bhai",
+        messageType: "text",
+        messageReceived: "false",
+        time: "2:59"
+      },
+      {
+        messageId: "msg2",
+        message: "need assistance",
+        messageType: "text nhi h",
+        messageReceived: "true",
+        time: "8:59"
+      }
+    ]
+  },
+  {
+    id: 14,
+    title: "Netra",
+    imageURL: "../p1.jpg",
+    orderId: "OD1234",
+    lastRead: "8:34",
+    messageList: [
+      {
+        messageId: "msg1",
+        message:
+          "text h toh h bhai text h toh h bhai text h toh h bhai text h toh h bhai text h toh h bhai",
+        messageType: "text",
+        messageReceived: "false",
+        time: "6:59"
+      },
+      {
+        messageId: "msg2",
+        message: "need assistance",
+        messageType: "text nhi h",
+        messageReceived: "false",
+        time: "20:59"
+      }
+    ]
+  },
+  {
+    id: 15,
+    title: "Sella",
+    imageURL: "../p2.jpg",
+    orderId: "OD1234",
+    lastRead: "8:34",
+    messageList: [
+      {
+        messageId: "msg1",
+        message: "heya good ppl",
+        messageType: "text",
+        messageReceived: "false",
+        time: "2:59"
+      },
+      {
+        messageId: "msg2",
+        message: "need assistance",
+        messageType: "text nhi h",
+        messageReceived: "true",
+        time: "8:59"
+      }
+    ]
+  }
+];
+
+function renderMessageList(chatMessagesContainer, dataId) {
+  chatMessagesContainer.innerHTML = "";
+  let messageList = JSON.parse(localStorage.getItem(Number(dataId)));
+
+  messageList.forEach((msg) => {
+    let singlemessage = document.createElement("div");
+    singlemessage.classList.add("singlemessage");
+
+    let getClass = msg.messageReceived === "true" ? "receivedmsg" : "sentmsg";
+    let eleclass =
+      msg.messageReceived === "true" ? "receivedcolor" : "sentcolor";
+    let ele = document.createElement("div");
+    singlemessage.classList.add(getClass);
+    ele.classList.add(eleclass);
+    let message = document.createElement("p");
+    message.classList.add("message");
+    let text = document.createTextNode(msg.message);
+    message.appendChild(text);
+    ele.appendChild(message);
+
+    let time = document.createElement("div");
+    let textOftime = document.createTextNode(msg.time);
+    time.classList.add("textOftime");
+    time.appendChild(textOftime);
+    ele.append(time);
+
+    // let getClass = msg.messageReceived==="true"?"receivedmsg":"sentmsg";
+
+    singlemessage.appendChild(ele);
+
+    chatMessagesContainer.appendChild(singlemessage);
+  });
+}
+
+function renderMessagesbox(rightSide, dataId) {
+  let chatMessagesContainer = document.createElement("div");
+  chatMessagesContainer.classList.add("chatMessagesContainer");
+
+  renderMessageList(chatMessagesContainer, dataId);
+
+  rightSide[0].appendChild(chatMessagesContainer);
+}
+
+function addMessage(dataId) {
+  let msg = document.getElementById("input-type");
+
+  let messageList = JSON.parse(localStorage.getItem(Number(dataId)));
+  console.log("@@", msg, msg.value, messageList);
+  let messageObj = {
+    messageId: "msg1",
+    message: msg.value,
+    messageType: "text",
+    messageReceived: "false",
+    time: "2:59"
+  };
+  let newObj = [...messageList, messageObj];
+  console.log("newobj", newObj);
+  localStorage.setItem(Number(dataId), JSON.stringify(newObj));
+  let chatbox = document.getElementsByClassName("chatMessagesContainer");
+  renderMessageList(chatbox[0], dataId);
+  msg.value = "";
+}
+
+function renderTypebox(rightSide, dataId) {
+  let typecontainer = document.createElement("div");
+  typecontainer.classList.add("search-container");
+
+  let inputbox = document.createElement("input");
+  inputbox.type = "text";
+  inputbox.id = "input-type";
+  inputbox.name = "inputbox";
+  inputbox.placeholder = "Type your message";
+  inputbox.classList.add("inputboxclass");
+  typecontainer.appendChild(inputbox);
+  let btnicon = document.createElement("div");
+  btnicon.classList.add("sendicon");
+  btnicon.innerHTML = '<ion-icon name="send-sharp"></ion-icon>';
+  typecontainer.appendChild(btnicon);
+  rightSide[0].appendChild(typecontainer);
+
+  btnicon.addEventListener("click", function () {
+    addMessage(dataId);
+  });
+}
+
+function renderRightSide(dataId) {
+  console.log("i am here", dataId);
+  let rightSide = document.getElementsByClassName("right-side");
+  rightSide[0].innerHTML = "";
+  let dataObj = data.filter((ele) => {
+    return ele.id.toString() === dataId;
+  });
+  let header = document.createElement("div");
+  header.classList.add("header");
+
+  let userImage = document.createElement("div");
+  userImage.classList.add("userImage");
+  let image = document.createElement("img");
+  image.src = dataObj[0].imageURL;
+  image.classList.add("usericon");
+
+  userImage.appendChild(image);
+
+  header.appendChild(userImage);
+
+  rightSide[0].appendChild(header);
+
+  renderMessagesbox(rightSide, dataId);
+
+  renderTypebox(rightSide, dataId);
+}
+
+(function getContacts() {
+  let element = document.getElementsByClassName("left-side");
+  console.log("##", element, element[0]);
+  let chatContainer = document.createElement("div");
+  chatContainer.classList.add("chatContainer");
+  data.forEach((ele) => {
+    let personChat = document.createElement("div");
+    personChat.classList.add("personchat");
+    personChat.setAttribute("dataId", ele.id);
+
+    let image = document.createElement("img");
+    image.src = ele.imageURL;
+    image.classList.add("imageClass");
+    personChat.appendChild(image);
+
+    let persondata = document.createElement("div");
+    persondata.classList.add("persondata");
+    let headerdata = document.createElement("div");
+    headerdata.classList.add("headerdata");
+
+    let name = document.createElement("p");
+    name.classList.add("name");
+    name.innerText = ele.title;
+    headerdata.appendChild(name);
+
+    let time = document.createElement("span");
+    let txt = document.createTextNode(ele.lastRead);
+    time.appendChild(txt);
+    time.classList.add("time");
+    headerdata.appendChild(time);
+
+    persondata.appendChild(headerdata);
+
+    let lastMessage = document.createElement("div");
+    lastMessage.classList.add("lastMessage");
+    lastMessage.innerText = ele.messageList[0].message;
+
+    persondata.appendChild(lastMessage);
+
+    personChat.appendChild(persondata);
+
+    chatContainer.appendChild(personChat);
+
+    localStorage.setItem(ele.id, JSON.stringify(ele.messageList));
+
+    personChat.addEventListener("click", function () {
+      console.log("pers33", personChat.getAttribute("dataId"));
+      renderRightSide(personChat.getAttribute("dataId"));
+    });
+  });
+  element[0].appendChild(chatContainer);
+})();
+
+//personchat - image headerdata
